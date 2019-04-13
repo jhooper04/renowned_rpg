0ebe21516ff5b411d231e840825c2d6ebded7b1a
Jake Hooper <jhooper04@users.noreply.github.com>
2019-04-13 14:10:28 -0500

    Cleanup debugging comments

--- renowned_rpg/crafts.lua
+++ renowned_rpg/crafts.lua
@@ -44,8 +44,6 @@ minetest.register_tool("renowned_rpg:leather_vessel", {
             contents = 0
         end
 
-        print("on use called (drink): "..pointed_thing.type)
-
         if contents > 0 and thirst < stats.thirst then
             local diff = stats.thirst - thirst
             if contents <= diff then
@@ -53,44 +51,27 @@ minetest.register_tool("renowned_rpg:leather_vessel", {
                 contents = 0 
                 meta:set_int("contents", contents)
                 itemstack:set_wear(0)
-                print("contents: "..tostring(contents))
             elseif contents > diff then
                 thirst = thirst + diff
                 contents = contents-diff
                 meta:set_int("contents", contents)
                 itemstack:set_wear(65535-math.floor(contents/def.capacity*65535))
-                print("contents: "..tostring(contents))
             end
             renowned_rpg:set_thirst(player, thirst)
             renowned_rpg:update_thirst_hud(player)
         end
-        
-        --print("drinking...")
-        
-        --if renowned_rpg:get_thirst(player) >= stats.hlth then
-            
-        --else
-        --    renowned_rpg:set_thirst(player, stats.thirst)
-        --    renowned_rpg:update_thirst_hud(player)
-        --end
 
         return itemstack
     end,
     on_place = function(itemstack, player, pointed_thing)
-        print("on place called (fill): "..pointed_thing.type)
-        
         local meta = itemstack:get_meta()
         local def = itemstack:get_definition()
         local under_pos = pointed_thing.under
         local node = minetest.get_node_or_nil(under_pos)
-        --local under_def = node and minetest.registered_nodes[node.name]
-        --print(dump(node))
-        --print(dump(under_def))
 
         if node.name == "default:water_source" then
             itemstack:set_wear(1)
             meta:set_int("contents", def.capacity)
-            print("capacity: "..tostring(def.capacity))
         end
 
         return itemstack
@@ -101,6 +82,6 @@ minetest.register_craft({
 	output = "renowned_rpg:leather_vessel",
 	recipe = {
 		{"mobs:leather", "",             "mobs:leather"},
-		{            "", "mobs:leather", ""},
+		{""            , "mobs:leather", ""            },
 	}
 })
